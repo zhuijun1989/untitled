bdffa269399044c2d0127446ebc4c99f823567c9
zhuijun1989 <47141802+zhuijun1989@users.noreply.github.com>
2020-07-30 09:27:40 +0800

    json file dff

--- btc_close_2017.py
+++ btc_close_2017.py
@@ -1,11 +1,26 @@
-from urllib.request import urlopen
+# from urllib.request import urlopen
 import json
+import requests
 
-#json_url = "https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json"
+#json_url = "https://github.com/zhuijun1989/untitled/blob/master/btc_close_2017.json"
+filename ='btc_close_2017.json'
+
+#'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
+
+
+with open(filename)as f:
+    btc_date = json.load(f)
+for btc_dict in btc_date:
+    date = btc_dict['date']
+    month = int(btc_dict['month'])
+    week = int(btc_dict['week'])
+    weekday = btc_dict['weekday']
+    close =int(btc_dict['close'])
+    print("{} is month {} week {},{},the close price is {} RMB".format(date,month,week,weekday,close))
+
 #response = urlopen(json_url)
-#req = response.read()
-
-with open('btc_close_2017.json','wb') as f:
-    f.write(q)
-    file_urllib = json.load(q)
-print(file_urllib)
+# req = requests.get(json_url)
+#
+# with open('btc_close_2017_request.json','w',encoding='utf-8') as f:
+#     f.write(req.text)
+# file_requests = req.json()
